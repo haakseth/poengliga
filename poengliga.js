405409c2aba57fc9af30ee09344de914082f1136
John Wika Haakseth <johnwika@gmail.com>
2017-03-06 07:22:12 +0100

    Initial commit

---
+++ poengliga.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var fs      = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+function scrapeStats(baseUrl, fileName, callback){
+  var urls = [];
+  for (var i = 300; i >= 0; i--) {
+    urls.push(
+      {
+        url: baseUrl + i,
+        id: i
+      }
+    );
+  }
+  var json = {
+    players: []
+  }
+  function scraper(count){
+    if(count < urls.length){
+      request(urls[count].url, function(error, response, html){
+        if(!error){
+          var $ = cheerio.load(html);
+  
+          var player = { 
+            playerInfo: {},
+            stats : []
+          };
+  
+          var rows = $('tr');
+          var playerDetailRow = rows[2];
+          var playerDetailsNodes = $(playerDetailRow).children()[1].childNodes;
+          var imageUrl = $($(playerDetailRow).children()[0].childNodes[1]).attr('src');
+          if(playerDetailsNodes[2].data !== "  "){
+            player.playerInfo.id          = urls[count].id;
+            player.playerInfo.name        = playerDetailsNodes[2].data;
+            player.playerInfo.imageUrl    = "http://poengliga.no/" + imageUrl;            
+            player.playerInfo.shirtNr     = playerDetailsNodes[6].data;
+            player.playerInfo.height      = playerDetailsNodes[10].data;
+            player.playerInfo.birthdate   = playerDetailsNodes[14].data;
+            player.playerInfo.position    = playerDetailsNodes[18].data;
+            player.playerInfo.teamName    = playerDetailsNodes[22].data;
+            player.playerInfo.reach       = playerDetailsNodes[26].data;
+            player.playerInfo.block       = playerDetailsNodes[30].data;
+            player.playerInfo.nation      = playerDetailsNodes[34].data;
+            player.playerInfo.intMatches  = playerDetailsNodes[38].data;
+            console.log(player.playerInfo.name);
+            for(var i = 0; i < rows.length; i++){
+              var row = rows[i];
+              var rowNodes = $(row).children();
+              if(rowNodes.length === 7 && 
+                $(rowNodes[0]).text() !== "Matchnr" &&
+                $(rowNodes[0]).text() !== ""){
+                player.stats.push({
+                  matchnr:      $(rowNodes[0]).text(),
+                  matchTeams:   $(rowNodes[1]).text(),
+                  result:       $(rowNodes[2]).text(),
+                  serve:        $(rowNodes[3]).text(),
+                  attack:       $(rowNodes[4]).text(),
+                  block:        $(rowNodes[5]).text(),
+                  total:        $(rowNodes[6]).text()
+                });
+              }
+            }
+            json.players.push(player);
+          }
+          
+          scraper(count+1);
+        }else{
+          console.log("error");
+          scraper(count+1);
+        }
+        
+      })
+    }else{
+      fs.writeFile(fileName, JSON.stringify(json, null, 4), function(err){
+        console.log('File written! ' + fileName);
+      })
+      callback(json);
+    } 
+  }
+  scraper(0);
+}
+
+app.get('/herrer/', function(req, res){
+  var baseUrl = 'http://poengliga.no/pl_player_show_detail.php?id=';
+  scrapeStats(baseUrl, 'herrer.json', function(json){
+    res.send(json);
+  });
+})
+
+app.get('/damer/', function(req, res){
+  var baseUrl = 'http://poengliga.no/pl_player_show_detail_w.php?id=';
+  scrapeStats(baseUrl, 'damer.json', function(json){
+    res.send(json);
+  });
+})
+
+app.listen('8081')
+console.log('kjører på localhost:8081');
+exports = module.exports = app;
---
+++ statsFromGames.js
@@ -0,0 +1,185 @@
+var express = require('express');
+var fs      = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+var iconv   = require('iconv'); 
+
+function statSorter(a,b){
+  if(b.totalt !== a.totalt){
+    return b.totalt - a.totalt;
+  }
+  if(b.angrep !== a.angrep){
+    return b.angrep - a.angrep;
+  }
+  if(b.serve !== a.serve){
+    return b.serve - a.serve;
+  }
+  if(b.blokk !== a.blokk){
+    return b.blokk - a.blokk;
+  }
+  return 0;
+  
+}
+function scrapeStats(baseUrl, fileName, callback){
+  var urls = [];
+  for (var i = 0; i < 100; i++) {
+    console.log(baseUrl + i + "web.html");
+    urls.push(
+      {
+        url: baseUrl + "/0607/kamper/" + i + "web.html",
+        id: i
+      }
+    );
+    urls.push(
+      {
+        url: baseUrl + "/0708/kamper/" + i + "web.html",
+        id: i
+      }
+    );
+    urls.push(
+      {
+        url: baseUrl + "/0809/kamper/" + i + "web.html",
+        id: i
+      }
+    );
+    urls.push(
+      {
+        url: baseUrl + "/0910/kamper/" + i + "web.html",
+        id: i
+      }
+    );
+    urls.push(
+      {
+        url: baseUrl + "/1011/kamper/" + i + "web.html",
+        id: i
+      }
+    );
+    urls.push(
+      {
+        url: baseUrl + "/1112/kamper/" + i + "web.html",
+        id: i
+      }
+    );
+    urls.push(
+      {
+        url: baseUrl + "/1213/kamper/" + i + "web.html",
+        id: i
+      }
+    );
+    urls.push(
+      {
+        url: baseUrl + "/1314/kamper/" + i + "web.html",
+        id: i
+      }
+    );
+    urls.push(
+      {
+        url: baseUrl + "/1415/kamper/" + i + "web.html",
+        id: i
+      }
+    );
+    urls.push(
+      {
+        url: baseUrl + "/1516/kamper/" + i + "web.html",
+        id: i
+      }
+    );
+    urls.push(
+      {
+        url: baseUrl + "/1617/kamper/" + i + "web.html",
+        id: i
+      }
+    );
+  }
+  var json = {
+    statistikker: []
+  }
+  function scraper(count){
+    if(count < urls.length){
+      request({url: urls[count].url, encoding: null}, function(error, response, html){
+        console.log(urls[count].url);
+        var length = "";
+        if(!error){
+          var ic = new iconv.Iconv('iso-8859-1', 'utf-8');
+          var convHtml = ic.convert(html);
+          var $ = cheerio.load(convHtml);
+          var rows = $("tr");
+          if(rows.length >0){
+            var lagARows = rows[3].childNodes;
+            var lagBRows = rows[4].childNodes;
+            var turnering = "";
+            var dato = "";
+            if(rows[1].childNodes.length == 2){
+              turnering = $(rows[1].childNodes[0]).text();
+              dato = $(rows[1].childNodes[1]).text();
+            }else{
+              turnering = $(rows[1]).text();
+            }
+            var kampInfo = {
+              turnering:  turnering.substr(15),
+              dato:       dato,
+              url:        urls[count].url,
+              motstandere: $(lagARows[1]).text() + " - " + $(lagBRows[1]).text(),
+              resultat:   $(lagARows[2]).text() + "-" + $(lagBRows[2]).text()
+            };
+            for (var i = 8; i < 26; i++) {
+              if(($(rows[i].childNodes[4]).text() !== "") && parseInt($(rows[i].childNodes[3]).text()) > 19){
+                json.statistikker.push({
+                  navn: $(rows[i].childNodes[4]).text(),
+                  klubb: $(lagARows[1]).text(),
+                  draktNr: i-7,
+                  serve: parseInt($(rows[i].childNodes[0]).text()),
+                  angrep: parseInt($(rows[i].childNodes[1]).text()),
+                  blokk: parseInt($(rows[i].childNodes[2]).text()),
+                  totalt: parseInt($(rows[i].childNodes[3]).text()),
+                  kampInfo: kampInfo
+                });
+              }
+              if($(rows[i].childNodes[6]).text() !== "" && parseInt($(rows[i].childNodes[10]).text()) > 19){
+                json.statistikker.push({
+                  navn: $(rows[i].childNodes[6]).text(),
+                  klubb: $(lagBRows[1]).text(),
+                  draktNr: i-7,
+                  serve: parseInt($(rows[i].childNodes[7]).text()),
+                  angrep: parseInt($(rows[i].childNodes[8]).text()),
+                  blokk: parseInt($(rows[i].childNodes[9]).text()),
+                  totalt: parseInt($(rows[i].childNodes[10]).text()),
+                  kampInfo: kampInfo
+                });
+              }
+            }
+          }
+
+          scraper(count+1);
+        }
+        
+      })
+    }else{
+      json.statistikker = json.statistikker.sort(statSorter);
+      fs.writeFile(fileName, JSON.stringify(json, null, 4), function(err){
+        console.log('File written! ' + fileName);
+      })
+      callback(json);
+    } 
+  }
+  scraper(0);
+}
+
+app.get('/herrer/', function(req, res){
+  var baseUrl = 'http://www.poengliga.no/eliteh';
+  scrapeStats(baseUrl, 'herrer.json', function(json){
+    res.send(json);
+  });
+})
+
+app.get('/damer/', function(req, res){
+  var baseUrl = 'http://www.poengliga.no/elited';
+  scrapeStats(baseUrl, 'damer.json', function(json){
+    res.send(json);
+  });
+})
+
+app.listen('8083')
+console.log('kjører på localhost:8083');
+exports = module.exports = app;
